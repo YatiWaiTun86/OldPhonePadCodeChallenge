8b40e60b7328af5c8ed42bd427bb59b3fc6fd0af
YatiWaiTun86 <yatiwaitun86@gmail.com>
2025-02-09 10:07:01 +0630

    Add project files.

---
+++ C# Code Challenge/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static string OldPhonePad(string input)
+    {
+        var keypad = new Dictionary<char, string>
+        {
+            {'1',"&'(" },{'2', "ABC"}, {'3', "DEF"}, {'4', "GHI"}, {'5', "JKL"},
+            {'6', "MNO"}, {'7', "PQRS"}, {'8', "TUV"}, {'9', "WXYZ"},
+            {'0', " "}
+        };
+
+        string result = "";
+        char lastKey = '\0'; 
+        int count = 0;         
+
+        foreach (char key in input)
+        {
+            if(lastKey == key)
+            {
+                count++;
+            } 
+            else if (key == '*')
+            { 
+                 if (result.Length > 0)
+                 {
+                    result = result.Substring(0, result.Length );
+                    count = 0;
+                    lastKey = '\0';  
+                 }
+                continue;
+            }
+            else if (key == '#')
+            {
+                break;
+            }
+            else
+            {
+                if (lastKey != '\0' && keypad.ContainsKey(lastKey))
+                {
+                    string group = keypad[lastKey];
+                    result += group[(count - 1) % group.Length];  
+                }
+
+                lastKey = key;
+                count = 1;  
+            }
+        }
+
+        if (lastKey != '\0' && keypad.ContainsKey(lastKey))
+        {
+            string group = keypad[lastKey];
+            result += group[(count - 1) % group.Length];
+        }
+
+
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        string result = OldPhonePad("557***#");
+        Console.WriteLine(result);
+
+    }
+}
---
+++ OldPhoneKeyTests/ProgramTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhoneKeypad.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void OldPhonePad_WhenGivenValidKeys_ReturnsCorrectOutput()
+        {
+            string input = "33#";
+            string expectedOutput = "E";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        public void OldPhonePad_WhenStarKeyIsPressed_ReturnsCorrectOutput()
+        {
+            string input = "227*#";
+            string expectedOutput = "B";
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        public void OldPhonePad_WhenSpaceKeyIsUsed_ReturnsCorrectOutput()
+        {
+            string input = "4433555 555666#";
+            string expectedOutput = "HELLO";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        public void OldPhonePad_WhenSpaceAndStarKeyAreUsed_ReturnsCorrectOutput()
+        {
+            string input = "8 88777444666*664#";
+            string expectedOutput = "TURING";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        public void OldPhonePad_WhenSingleInputExceedsCount_ReturnsCorrectOutput()
+        {
+            string input = "77777#";
+            string expectedOutput = "P";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
